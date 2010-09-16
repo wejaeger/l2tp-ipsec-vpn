8b1ae478fce52ff5b614dba0ac9a7ab16c636599
werner <werner@b6315ce2-5a64-4426-9aac-a11868b78efd>
2010-09-16 07:40:27 +0000

    improved packaging

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -55,7 +55,7 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=
+LDLIBSOPTIONS=-s
 
 nbproject/qt-${CND_CONF}.mk: nbproject/qt-${CND_CONF}.pro FORCE
 	${QMAKE} VPATH=. -o qttmp-${CND_CONF}.mk nbproject/qt-${CND_CONF}.pro
