8490eaf6347643199a6864168968d852caf35261
werner <werner@b6315ce2-5a64-4426-9aac-a11868b78efd>
2010-09-17 09:12:39 +0000

    added copyright and license information to source file comment

--- trunk/src/util/EncSecrets.cpp
+++ trunk/src/util/EncSecrets.cpp
@@ -5,14 +5,21 @@
  * Author: Werner Jaeger
  *
  * Created on July 28, 2010, 10:48 AM
- */
-
-/*
- * $Id$
  *
- * EncSecrets Class
- * Class author: Werner Jäger).
- * (comments optimized for using DOC++)
+ * Copyright 2010 Werner Jaeger.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <cstring>       // strlen, bzero
